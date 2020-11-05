527639b472daf1788879a329979db7654105e6a4
rick2785 <rjhrabowskie@gmail.com>
2020-11-05 16:52:31 -0600

    Initial Commit

---
+++ CurrencyExchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyExchange
+//
+//  Created by RJ Hrabowskie on 11/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by RJ Hrabowskie on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
